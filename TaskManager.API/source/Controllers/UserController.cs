996c96a26e6460629e1fc857074db8352cfda8e4
SulaimonYNWA <sulajmoninomov@gmail.com>
2025-04-03 16:01:04 +0300

    get projects by userID

--- TaskManager.API/source/Controllers/UserController.cs
+++ TaskManager.API/source/Controllers/UserController.cs
@@ -24,4 +24,19 @@ public class UsersController : ControllerBase
         var users = await connection.QueryAsync<User>("SELECT * FROM users;");
         return Ok(users);
     }
+    
+    [HttpGet("{username}")]
+    public async Task<ActionResult<User>> GetUserByUsername(string username)
+    {
+        using var connection = new MySqlConnection(_connectionString);
+        
+        var user = await connection.QueryFirstOrDefaultAsync<User>(
+            "SELECT * FROM users WHERE username = @Username", new { Username = username });
+
+        if (user == null)
+            return NotFound(new { message = "User not found" });
+
+        return Ok(user);
+    }
+    
 }
